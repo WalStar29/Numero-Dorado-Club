4ee56391ee40f6fc426d4d81aeb63d281af25fa7
WalStar29 <castrowalter172913@hotmail.com>
2025-10-11 16:10:40 -0400

    Quita output: export para permitir rutas API

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true, // 🔥 Esto desactiva la optimización automática
   },
