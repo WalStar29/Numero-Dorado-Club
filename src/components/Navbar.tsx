02f30a4c01692b2105b567c6bda0eb8c3f7eb54a
WalStar29 <castrowalter172913@hotmail.com>
2025-10-18 21:00:52 -0400

    ✨ Navbar: sección de redes sociales con íconos personalizados y enlaces activos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,21 +19,33 @@ export default function Navbar() {
                 <div className="navbar-logo">
                     <img src="/loaders.svg" alt="Logo Número Dorado Club" className="logo-img" />
                 </div>
-
-                {/* Ícono hamburguesa */}
                 <button className="navbar-menu-icon" onClick={() => setMenuAbierto(true)}>
-                <FaBars />
+                    <FaBars />
                 </button>
             </div>
 
-            {/* Menú lateral móvil */}
             <div className={`navbar-sidebar ${menuAbierto ? 'activo' : ''}`}>
                 <button className="navbar-close-icon" onClick={() => setMenuAbierto(false)}>
-                <FaTimes />
+                    <FaTimes />
                 </button>
+                <div className="navbar-social-section">
+                    <h3 className="navbar-social-title">Nuestras redes sociales</h3>
+                    <div className="navbar-icon-links">
+                        <a href="#" className="navbar-icon-item">
+                            <img src="/youtube.svg" alt="YouTube" className="navbar-icon" />
+                            <span>YouTube</span>
+                        </a>
+                        <a  href="https://wa.me/584223939612" target="_blank" className="navbar-icon-item">
+                            <img src="/what.svg" alt="WhatsApp" className="navbar-icon" />
+                            <span>WhatsApp</span>
+                        </a>
+                        <a href="https://www.instagram.com/numerodoradoclub?igsh=MWg5cmhhZmU3ZDk5YQ==" target="_blank" className="navbar-icon-item">
+                            <img src="/insta.svg" alt="Instagram" className="navbar-icon" />
+                            <span>Instagram</span>
+                        </a>
+                    </div>
+                </div>
             </div>
-
-            {/* Fondo oscuro sin interacción */}
             {menuAbierto && <div className="navbar-overlay" />}
         </nav>
     )
